85a84ec1736a3e5b92720e2ed11ac61550b8d6ec
Alena Maarouf <sunnysandyalena@macbook-air-3.myfiosgateway.com>
2023-01-07 14:25:43 -0500

    Design changes

--- src/App.js
+++ src/App.js
@@ -9,17 +9,30 @@ import Shop from './Shop';
 import Home from './Home';
 import Favorites from './Favorites';
 import Cart from './Cart';
+import Account from "./Account";
 import './App.css'; 
 import logo from './dazzling.png';
+import contactImg from './contact-info-pink-icon.png';
+import shopImg from './shop-icon.png';
+import favImg from './loves-pink-icon.png';
+import cartImg from './cart-pink-icon.png';
+import homeImg from './home-icon.png';
+import accountImg from './account.png';
 
 function App() {
   return <Router>
     <nav>
-      <Link to='/Contact' className='link'>CONTACT</Link>
-      <Link to='/Shop' className='link'>SHOP</Link>
-      <Link to='/'><img className="logo-img" src={logo} alt = 'Dazzling Logo'/></Link>
-      <Link to='/Favorites' className='link'>FAVORITES</Link>
-      <Link to='/Cart' className='link'>CART</Link>
+      <div>
+        <Link to='/' className="link"><span className="logo">Dazzling</span><span><img className="logo-img" src={logo} alt = 'Dazzling Logo'/></span><span className="logo">Jewelry</span></Link>
+      </div>
+      <div>
+        <Link to='/' className="link"><img className="navIcon" src = {homeImg} alt = 'Home Page'/></Link>
+        <Link to='/Contact' className='link'><img className="navIcon" src = {contactImg} alt = 'Contact'/></Link>
+        <Link to='/Shop' className='link'><img className="navIcon" src = {shopImg} alt = 'Shop'/></Link>
+        <Link to='/Favorites' className='link'><img className="navIcon" src = {favImg} alt = 'Favorites'/></Link>
+        <Link to='/Cart' className='link'><img className="navIcon" src = {cartImg} alt = 'Cart'/></Link>
+        <Link to='/Account' className="link"><img className="navIcon" src = {accountImg} alt = 'Account'/></Link>
+      </div>
     </nav>
     <Routes>
       <Route path='/Contact' element={<Contact/>}/>
@@ -27,6 +40,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/Favorites' element={<Favorites/>}/>
       <Route path='/Cart' element={<Cart/>}/>
+      <Route path='./Account' element={<Account/>}/>
     </Routes>
   </Router>
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,3 @@
-import home from './home.png';
 import email from './email.png';
 import phone from './phone.png';
 import instagram from './instagram.png';
@@ -6,12 +5,9 @@ import instagram from './instagram.png';
 function Footer() {
     return (
         <div className='container-footer'>
-            <div><img className='footer-icon' src = {home} alt = 'Home Page'/></div>
-            <div className='container-icons'>
-                <div><img className='footer-icon' src = {email} alt = 'Email'/></div>
-                <div><img className='footer-icon' src = {phone} alt = 'Phone'/></div>
-                <div><img className='footer-icon' src = {instagram} alt = 'Instagram'/></div>
-            </div>
+            <div><img className='footer-icon' src = {email} alt = 'Email'/></div>
+            <div><img className='footer-icon' src = {phone} alt = 'Phone'/></div>
+            <div><img className='footer-icon' src = {instagram} alt = 'Instagram'/></div>
         </div>
     )
 }
